03b97c243e1f55b639df3423e8c759a8661d7950
Vedran Milković <vedran.milkovic25@gmail.com>
2021-07-10 21:31:38 +0200

    The Context System with React

---
+++ translate/src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')}></i>
+          <i className="flag hr" onClick={() => this.onLanguageChange('croatian')}></i>
+        </div>
+        <LanguageContext.Provider value={this.state.language}>
+          <ColorContext.Provider value="primary">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
